7501b7abe2e1f76bf818f83438f0d4ce4593ce97
maks0 <maks0_00@inbox.ru>
2021-06-11 10:54:33 +0500

    Удален BearUi

--- BearBundle/BearUI/Shaders/imgui.ps.hlsl
+++
@@ -1,8 +0,0 @@
-#include "common.h.hlsl"
-BINDING_SRV_0 Texture2D t_basic_color : register(t0);
-BINDING_SAMPLER_0 SamplerState sdefault : register(s0);
-
-float4 main(PS_IMGUI_IN input) : SV_Target
-{
-    return t_basic_color.Sample(sdefault, input.uv)*input.color;
-}
